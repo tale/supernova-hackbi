1b36f736370337305e54f6f30fc0cbff1fc4b183
AnonymousCheung <AnonymousCheung@users.noreply.github.com>
2021-01-23 16:00:48 -0500

    Create Player.cs

---
+++ Supernova/Code/Object/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SuperNova.Code.Object {
+
+    public class Player {
+
+        private Vector2 position, scale;
+
+        private Texture2D sprite;
+
+        public Player(Vector2 position, Vector2 scale) {
+
+            this.position = position;
+            this.scale = scale;
+            this.sprite = makePlayerTexture();
+        }
+
+        public void tick() {
+
+        }
+
+        public void render() {
+
+
+
+        }
+
+        private static Texture2D makePlayerTexture() {
+
+
+        }
+    }
+
+}
