8ae047a6f1419c9f7f2528c4c4dea878316bb87f
Peter Downey <peter.d.downey@gmail.com>
2021-01-23 14:29:52 -0500

    adding some basic stuff to main

--- Supernova/Code/SupernovaGame.cs
+++ Supernova/Code/SupernovaGame.cs
@@ -1,17 +1,26 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.IO;
+
+using Fondue.Code.Utilities;
 
 namespace Supernova.Code
 {
     public class SupernovaGame : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+
+        public const int STARTSCREEN = 0, GAMESCREEN = 1, MENUSCREEN = 2, SETTINGSCREEN = 3;
+        public int gameState = GAMESCREEN;
+
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
 
         public SupernovaGame()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -19,25 +28,56 @@ namespace Supernova.Code
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferHeight = 450;
+            graphics.PreferredBackBufferWidth = 720;
+
+            Window.AllowUserResizing = true;
+            Window.IsBorderless = false;
+
+            graphics.ApplyChanges();
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
         }
 
-        protected override void Update(GameTime gameTime)
-        {
+        protected override void Update(GameTime gameTime) {
+
+            Camera.update(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+
+            MouseState mouseState = Mouse.GetState();
+            KeyboardState keyBoardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            switch (gameState) {
 
-            base.Update(gameTime);
+                case STARTSCREEN:
+
+                    break;
+
+                case MENUSCREEN:
+
+                    break;
+
+                case SETTINGSCREEN:
+
+
+                    break;
+
+                
+
+
+                    // TODO: Add your update logic here
+
+                    base.Update(gameTime);
+            }
         }
 
         protected override void Draw(GameTime gameTime)
--- Supernova/Code/Util/Camera.cs
+++ Supernova/Code/Util/Camera.cs
@@ -1,33 +1,65 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+//@author Peter Downey
 
 
-public static class Camera {
+namespace Fondue.Code.Utilities {
 
-    static Vector vector = new Vector();
+    public static class Camera {
 
-public static Vector getPosition() {
-    return vector;
-}
 
-public static float getXPosition() {
-    return vector.getX();
-}
+        public static readonly int WIDTH = 720, HEIGHT = 450;
 
-public static float getYPosition() {
-    return vector.getY();
-}
+        public static int currWidth = WIDTH, currHeight = HEIGHT;
 
-public static void addToPosition(Vector addition) {
-    vector.setX(vector.getX() + addition.getX());
-    vector.setY(vector.getY() + addition.getY());
-}
+        public static Vector2 flippedPlayerCords = Vector2.Zero;
 
-public static void addToX(float x) {
-    vector.setX(vector.getX() + x);
-}
+        public static void update(int currWidth, int currHeight) {
 
-public static void addToY(float y) {
-    vector.setY(vector.getY() + y);
-}
+            Camera.currWidth = currWidth;
+            Camera.currHeight = currHeight;
+        }
 
+        public static int getX() {
+
+            return (int)flippedPlayerCords.X;
+        }
+
+        public static void setX(float x) {
+
+            flippedPlayerCords.X = x;
+        }
+
+        public static int getY() {
+
+            return (int)flippedPlayerCords.Y;
+        }
+
+        public static void setY(float y) {
+
+            flippedPlayerCords.Y = y;
+        }
+
+        public static Vector2 getPoint() {
+
+            return new Vector2(flippedPlayerCords.X + currWidth - WIDTH, flippedPlayerCords.Y + currHeight - HEIGHT);
+        }
+
+        public static float getWidthScaler() {
+
+            return (float)currWidth / WIDTH;
+        }
+
+        public static float getHeightScaler() {
+
+            return (float)currHeight / HEIGHT;
+        }
+
+        public static bool isOnScreen(Vector2 position, Vector2 dimensions) {
+
+            return (position.X + Camera.getX() - dimensions.X / 2 < WIDTH && position.X + Camera.getX() + dimensions.X / 2 > 0 && position.Y + Camera.getY() - dimensions.Y / 2 < HEIGHT && position.Y + Camera.getY() + dimensions.Y / 2 > 0);
+        }
+    }
 }
