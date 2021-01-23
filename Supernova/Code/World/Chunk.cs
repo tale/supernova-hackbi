9c6ded524330a7642a7844d80b3dae22763c4af6
Peter Downey <peter.d.downey@gmail.com>
2021-01-23 17:11:17 -0500

    world managment logic

---
+++ Supernova/Code/World/Chunk.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SuperNova.Code.Object;
+
+//@author Peter Downey
+
+namespace Supernova.Code.World {
+    public class Chunk {
+
+        private static Random rand = new Random();
+
+      
+        protected Vector2 position = Vector2.Zero;
+        protected Planet[] planets;
+
+
+        public Chunk(float x, float y) {
+
+            position.X = x;
+            position.Y = y;
+
+            planets = new Planet[0];
+      
+        }
+
+
+
+        public void render(SpriteBatch _spritebatch) {
+
+            for (int n = 0; n < planets.Length; n++) {
+
+                planets[n].render(_spritebatch);
+            }
+        }
+    }
+}
+
+
+        
--- Supernova/Code/World/WorldManager.cs
+++ Supernova/Code/World/WorldManager.cs
@@ -1,5 +1,51 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using SuperNova.Code.Util;
+
 namespace Supernova.Code.World {
-    public class WorldManager {
-        
+    public static class WorldManager {
+
+        private static Dictionary<(int, int), Chunk> chunks = new Dictionary<(int, int), Chunk>();
+
+
+
+        public static void WorldTick() {
+
+
+            int cordX = -Camera.GetX();
+            int cordY = -Camera.GetY();
+
+            cordX = (cordX < 0 ? cordX -= 5000 : cordX) / 5000;
+            cordY = (cordY < 0 ? cordY -= 5000 : cordY) / 5000;
+
+            for (int j = -1; j <= 1; j++) {
+
+                for (int n = -1; n <= 1; n++) {
+
+                    if (!chunks.ContainsKey((cordX + n, cordY + j)))
+                        chunks.Add((cordX + n, cordY + j), new Chunk((cordX + n) * 5000, (cordY + j) * 5000));
+
+
+                }
+            }
+
+  
+        }
+
+        public static void WorldRender(SpriteBatch _spriteBatch) {
+
+            for (int j = -1; j <= 1; j++) {
+
+                for (int n = -1; n <= 1; n++) {
+
+                    chunks[(0, 0)].render(_spriteBatch);
+
+                }
+            }
+
+        }
+
+
+
     }
 }
